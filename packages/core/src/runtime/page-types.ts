29cab27dd141e35f7f8b2daaaea3782a2ed07816
T. Kameyama <tkamenoko@vivaldi.net>
2025-07-03 00:42:01 +0900

    feat: home template [WIP]

--- packages/core/src/runtime/page-types.ts
+++ packages/core/src/runtime/page-types.ts
@@ -1,5 +1,8 @@
 import type { FC, PropsWithChildren } from "react";
 
+export type PageProps = {};
+export type Page = FC<PageProps>;
+
 export type LayoutProps = PropsWithChildren<{}>;
 export type Layout = FC<LayoutProps>;
 
