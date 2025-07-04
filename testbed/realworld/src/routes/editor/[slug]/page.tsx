e58a87c87825736279254a4d47271c4c211a72ed
T. Kameyama <tkamenoko@vivaldi.net>
2025-07-04 20:14:55 +0900

    feat: add editor template [WIP]

---
+++ testbed/realworld/src/routes/editor/[slug]/page.tsx
@@ -0,0 +1,8 @@
+import type { Page } from "vrmf";
+import { ArticleEditor } from "../_articleEditor";
+
+const UpdateArticle: Page = () => {
+  return <ArticleEditor />;
+};
+
+export default UpdateArticle;
---
+++ testbed/realworld/src/routes/editor/page.tsx
@@ -0,0 +1,8 @@
+import type { Page } from "vrmf";
+import { ArticleEditor } from "./_articleEditor";
+
+const CreateArticle: Page = () => {
+  return <ArticleEditor />;
+};
+
+export default CreateArticle;
